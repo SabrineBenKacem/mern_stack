536f1bc11e166da1d858aa596de1ee2752bb0b20
Sabrine Ben Kacem <sabrinebenkacem@gmail.com>
2023-09-04 12:34:36 +0100

    first_jsx

---
+++ Week1/Day 3/Core/first_jsx/first_jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Hello Dojo!</h1>
+      <h5>Things I need to do</h5>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>feed the dogs</li>
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
